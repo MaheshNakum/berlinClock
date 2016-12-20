5eda27fa71a7556d87e86d208d276409646217bb
maheshnakum <mahesh>
2016-12-20 11:46:23 +0000

    Updated test case

--- src/test/java/SecondTest.java
+++ src/test/java/SecondTest.java
@@ -1,5 +1,7 @@
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created by maheshnakum on 13/12/2016.
@@ -10,11 +12,12 @@ public class SecondTest {
     public void convertSecond() throws Exception {
 
         int second1 = 10;
+        String result = "Y";
 
         Second second = new Second(second1);
 
-
-        Assert.assertEquals("Y", second.convertSecond());
+        //Assert.assertEquals("Y", second.convertSecond());
+        assertThat(second.convertSecond(), is(result));
 
     }
 
